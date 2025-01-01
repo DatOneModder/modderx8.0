7b68d5ef76828a63a5c23260025a0e597d742b9e
thatonemodder <140116471+DatOneModder@users.noreply.github.com>
2025-01-01 18:50:58 -0500

    Update Features.cs

--- Features.cs
+++ Features.cs
@@ -1,1 +1,89 @@
+// thanks for checking out these awesome features!
 
+Here is the lists of all the mods from ModderX8.0 BETA:
+
+FIND ALL DIFFERENT METHODS OF A BUTTON INSIDE []'s
+
+EX. FLY [SLOW, MEDIUM, FAST]
+
+-removed handtaps loud & silent
+---
+
+# Main Mods
+1. **Settings**
+2. **Join Random Public**
+3. **ANTI-REPORT**
+4. **Platforms: [REGULAR, STICKY, INVIS]**
+5. **Speedboost: [MOSA, SUPER]**
+6. **Noclip**
+7. **Fly: [SLOW, REGULAR, FAST]**
+8. **Ghost-Monke: [TOGGLE, HOLD]**
+9. **Invis-Monke: [TOGGLE, HOLD]**
+10. **Slide-Control: [WEAK, STRONG]**
+11. **Handtaps: [NORMAL, SPAZ]**
+12. **Chams: [PLR COLOR, INFECTION]**
+13. **Wall-Walk: [WEAK, REGULAR, STRONG]**
+14. **SlipperyWalls**
+15. **Long-Arms: [MOSA, GHOST]**
+16. **HeadSpinny: OFF**
+17. **WakyMonky: [CONSTANT, TOGGLE, HOLD]**
+18. **NoTagFreeze: OFF**
+19. **Fingers: [TRACKING, OFF]**
+19. **FlickTag[RT]: OFF**
+20. **Tag-Gun [TAGGED]**
+21. **Tag-Aura: [SHORT, LONG]**
+22. **ScareGun: [SLOW, REGULAR, FAST]**
+23. **JUMPSCARE**
+24. **TimeOfDay: [DAWN, DAY, NIGHT]**
+25. **WEATHER: [CLEAR, RAIN, SNOW]**
+26. **Network Triggers: ON**
+27. **Forest-Leaves: ON**
+28. **Spammers** (SEE LINE 59 FOR ALL SPAMMERS)
+
+---
+
+## Menu Settings
+1. **Return to Main**
+2. **Menu Button: [PRIMARY, SECONDARY]**
+3. **Platforms Button: [GRIPS, TRIGS, PRIMARY]**
+4. **Noclip Button: [L-TRIG, R-TRIG]**
+5. **Fly Button: [L-TRIG, R-PRIMARY]**
+6. **Ghost Button: [L&R-SECOND, L&R-PRIMARY, L-TRIG]**
+7. **Invis Button: [L-TRIG, L-PRIM, L-SECOND, R-SECOND, R-PRIM]**
+8. **WackyMonky Button: [L&R-TRIG, L&R-PRIMARY]**
+9. **WallWalk Button: [GRIP, TRIGGER, NONE]**
+10. **Right Hand**
+11. **FPS Counter**
+12. **Disconnect Button**
+
+---
+
+### Sound Spammers
+1. **Return to Main**
+2. **Button Toggle: [NW]**
+3. **WoodTap1: OFF**
+4. **WoodTap2: OFF**
+5. **WoodTap3: OFF**
+6. **WoodTap4: OFF**
+7. **CrystalTap: OFF**
+8. **CrystalTap2: OFF**
+9. **CrystalTap3: OFF**
+10. **MetalTap1: OFF**
+11. **MetalTap2: OFF**
+12. **GrassTap: OFF**
+13. **SqueakSound1: OFF**
+14. **SqueakSound2: OFF**
+15. **SqueakSound3: OFF**
+16. **TurkeySound: OFF**
+17. **CarpetTap: OFF**
+18. **PopSound: OFF**
+19. **SandTap: OFF**
+20. **BellSound: OFF**
+21. **BellSound2: OFF**
+22. **Umbrella_Open: OFF**
+23. **ScarySound: OFF**
+24. **Static1: OFF**
+25. **Static2: OFF**
+26. **Static3: OFF**
+27. **Static4: OFF**
+28. **Static5: OFF**
